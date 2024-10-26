5a814ae328c1fc3a4ebd3f1fb77c11d21960a83b
SkidRow <51795604+RolandoHidalgo@users.noreply.github.com>
2024-10-26 03:27:43 -0400

    Update index.delete.ts

--- server/api/skills/index.delete.ts
+++ server/api/skills/index.delete.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(async event => {
     const body = await readBody(event)
-    await useDrizzle().delete(tables.usersSkills).where(and(eq(tables.usersSkills.skillId, body.skillId), eq(tables.usersSkills.userId, 1)))
+    await useDrizzle().delete(tables.usersSkills).where(and(eq(tables.usersSkills.skillId, body.skillId), eq(tables.usersSkills.userId, 1))).returning().get()
     return body.skillId;
 })
