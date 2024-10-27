8da2ddbb96340a2d6713ed1f20f148f530ba8ecc
SkidRow <51795604+RolandoHidalgo@users.noreply.github.com>
2024-10-27 01:05:56 -0400

    trying to fix production error on delete skill

--- server/api/skills/[id].delete.ts
+++ server/api/skills/[id].delete.ts
@@ -1,5 +1,24 @@
-export default defineEventHandler(async event => {
-    const body = await readBody(event)
-    await useDrizzle().delete(tables.usersSkills).where(and(eq(tables.usersSkills.skillId, body.skillId), eq(tables.usersSkills.userId, 1))).returning().get()
-    return body.skillId;
+import {z} from "zod";
+
+export default defineEventHandler(async (event) => {
+    const {id: skillId} = await getValidatedRouterParams(event, z.object({id: z.coerce.number()}).parse)
+    const {user} = await requireUserSession(event);
+
+
+    const deletedSkill = await useDrizzle()
+        .delete(tables.usersSkills)
+        .where(and(
+            eq(tables.usersSkills.skillId, skillId),
+            eq(tables.usersSkills.userId, 1)
+        )).returning({id: tables.usersSkills.skillId})
+        .get()
+
+    if (!deletedSkill) {
+        throw createError({
+            statusCode: 404,
+            message: 'Todo not found'
+        })
+    }
+    return deletedSkill;
+
 })
