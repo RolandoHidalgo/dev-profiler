0ab6453585c36eae38406073fdbf79ee700cd887
SkidRow <51795604+RolandoHidalgo@users.noreply.github.com>
2025-12-25 09:01:39 -0500

    migrate3

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,12 +1,12 @@
-import { drizzle } from 'drizzle-orm/d1'
+
 export { sql, eq, and, or } from 'drizzle-orm'
 
-import * as schema from '../db/schema'
-import { db} from 'hub:db'
+
+import { db,schema} from 'hub:db'
 export const tables = schema
 
 export function useDrizzle() {
-    return drizzle(db, { schema })
+    return db
 }
 
 export type User = typeof schema.users.$inferSelect
